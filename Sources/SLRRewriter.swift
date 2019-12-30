6047f9460f367ad66d3650e77cdc5a74991c0b4d
AVVS <v@makeomatic.ca>
2019-12-30 12:46:11 -0800

    feat: be able to rename import declarations

--- Sources/SLRRewriter.swift
+++ Sources/SLRRewriter.swift
@@ -121,11 +121,13 @@ class SLRPublicRewriter: SyntaxRewriter {
   let identifiers: Set<String>!
   let fnIdentifiers: Set<SLRFuncReport>!
   let prefix: String!
+  let imports: [String]!
 
-  init(ids: Set<String>, prefix: String, fnIdentifiers: Set<SLRFuncReport>) {
+  init(ids: Set<String>, prefix: String, fnIdentifiers: Set<SLRFuncReport>, imports: [String]) {
     self.identifiers = ids
     self.prefix = prefix
     self.fnIdentifiers = fnIdentifiers
+    self.imports = imports
   }
 
   override func visit(_ token: TokenSyntax) -> Syntax {
@@ -147,11 +149,22 @@ class SLRPublicRewriter: SyntaxRewriter {
 
     return super.visit(node.withIdentifier(node.identifier.withKind(.identifier(prefix + node.identifier.text))))
   }
+
+  override func visit(_ node: ImportDeclSyntax) -> DeclSyntax {
+    guard node.path.count > 0, let name = node.path.firstToken?.text, imports.contains(name) else {
+      return super.visit(node)
+    }
+    
+    guard let firstPart = node.path.children.first(where: { _ in true }) as? AccessPathComponentSyntax else {
+      return super.visit(node)
+    }
+  
+    return super.visit(node.withPath(node.path.replacing(childAt: 0, with: firstPart.withName(firstPart.name.withKind(.identifier(prefix + name))))))
+  }
 }
 
-func rewrite(_ urls: [URL],
-             prefix: String,
-             reports: [SLRIdentifiersReport]? = nil) throws -> (syntax: [Syntax], identifiers: Set<String>, fnReplace: Set<SLRFuncReport>) {
+func rewrite(_ urls: [URL], prefix: String, reports: [SLRIdentifiersReport]? = nil) throws
+  -> (syntax: [Syntax], identifiers: Set<String>, fnReplace: Set<SLRFuncReport>) {
   var sources = [SourceFileSyntax]()
   var response = [Syntax]()
   var syntaxVisitor = FindPublicAndOpenExports()
@@ -169,12 +182,14 @@ func rewrite(_ urls: [URL],
 
   let baseRewriter = SLRPublicRewriter(ids: syntaxVisitor.replace,
                                        prefix: prefix,
-                                       fnIdentifiers: syntaxVisitor.fnReplace)
+                                       fnIdentifiers: syntaxVisitor.fnReplace,
+                                       imports: [])
 
   let rewriters = reports?.reduce(into: [baseRewriter], { res, rep in
     res.append(SLRPublicRewriter(ids: Set(rep.identifiers),
                                  prefix: rep.prefix,
-                                 fnIdentifiers: Set(rep.fnReplace)))
+                                 fnIdentifiers: Set(rep.fnReplace),
+                                 imports: rep.products ?? []))
   }) ?? [baseRewriter]
 
   for idx in urls.indices {
